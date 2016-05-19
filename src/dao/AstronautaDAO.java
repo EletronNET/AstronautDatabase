628da95f8a41cf9f0f460fa256488911e7fc2347
Erasmo de Castro Leite Junior <eleitejr@gmail.com>
2016-05-19 19:49:12 -0300

    Update AstronautaDAO.java

--- src/dao/AstronautaDAO.java
+++ src/dao/AstronautaDAO.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -54,8 +55,8 @@ public class AstronautaDAO {
 			stmt.setString(6, astronauta.getEstado_Nasc());
 			stmt.setString(7, astronauta.getCidade_Nasc());
 			stmt.setString(8, astronauta.getSexo());
-			stmt.setDate(9, astronauta.getDtNasc());
-			stmt.setDate(10, astronauta.getDtFalec());
+			stmt.setDate(9, Date.valueOf(astronauta.getDtNasc()));
+			stmt.setDate(10, Date.valueOf(astronauta.getDtFalec()));
 			stmt.setString(11, astronauta.getFoto());
 			stmt.setString(12, astronauta.getInfo());
 			stmt.setString(13, astronauta.getInfo_eng());
@@ -67,7 +68,7 @@ public class AstronautaDAO {
 			for (int k = numMissoes; k < 8; k++){
 				stmt.setString(14 + k, null);
 			}
-			
+
 			stmt.setBlob(22, astronauta.getImagem());
 
 			stmt.execute();
@@ -76,10 +77,10 @@ public class AstronautaDAO {
 	}
 
 	public List<Astronauta> lista() throws SQLException, NumberFormatException {
-		
+
 		int id;
-		
-		String 	nome, 
+
+		String 	nome,
 				nome_m,
 				sobrenome,
 				pais,
@@ -89,15 +90,15 @@ public class AstronautaDAO {
 				foto,
 				info,
 				info_eng;
-		
-		Date 	dataNasc, dataFalec;
-		
+
+		LocalDate 	dataNasc, dataFalec = LocalDate.of(1900,1, 1);
+
 		int n_missoes = 0;
-		
+
 		Blob	imagem = null;
 
 		List<Astronauta> astronautas = new ArrayList<>();
-		
+
 		String sql = "SELECT * FROM astronauta";
 
 		Connection con = ConnectionFactory.getConnection();
@@ -115,45 +116,45 @@ public class AstronautaDAO {
 					cidade = rs.getString("Cidade_Nasc");
 					estado = rs.getString("Estado_Nasc");
 					sexo = rs.getString("Sexo");
-					dataNasc = rs.getDate("DtNasc");
-					dataFalec = rs.getDate("DtFalec");
+					dataNasc = rs.getDate("DtNasc").toLocalDate();
+					dataFalec = rs.getDate("DtFalec").toLocalDate();
 					foto = rs.getString("Foto");
 					info = rs.getString("Info");
 					info_eng = rs.getString("Info_eng");
-														
-					ArrayList<String> missoes = new ArrayList<String>();
+
+					ArrayList<String> missoes = new ArrayList<>();
 
 					for (int m = 1; m <= 8; m++){
 						String mission = rs.getString("m" + (m));
 						missoes.add(mission);
-						
-					n_missoes = rs.getInt("N_missoes");
-						
-					imagem = rs.getBlob("Imagem");
+
+						n_missoes = rs.getInt("N_missoes");
+
+						imagem = rs.getBlob("Imagem");
 					}
 
 					// falta inserir campos de foto e biografia
 
 					Astronauta astronauta = new Astronauta(
-							
-								id, 		// int
-								nome, 		// String
-								nome_m, 	// String
-								sobrenome, 	// String
-								pais, 		// String
-								estado, 	// String
-								cidade,		// String
-								foto,		// String
-								sexo, 		// String
-								info,		// String
-								info_eng,	// String
-								dataNasc, 	// sql.Date
-								dataFalec,  // sql.Date
-								missoes,	// ArrayList<String>
-								n_missoes,  // int
-								imagem		// sql.Blob
+
+							id, 		// int
+							nome, 		// String
+							nome_m, 	// String
+							sobrenome, 	// String
+							pais, 		// String
+							estado, 	// String
+							cidade,		// String
+							foto,		// String
+							sexo, 		// String
+							info,		// String
+							info_eng,	// String
+							dataNasc, 	// sql.Date
+							dataFalec,  // sql.Date
+							missoes,	// ArrayList<String>
+							n_missoes,  // int
+							imagem		// sql.Blob
 					);
-					
+
 					astronauta.setIdAstronauta(id);
 					astronautas.add(astronauta);
 				}
@@ -161,123 +162,135 @@ public class AstronautaDAO {
 		}
 		return astronautas;
 	}
-	
+
 	public ArrayList<Astronauta> pegaAstronautas()
-			throws SQLException {
+			throws SQLException, NullPointerException {
 		ArrayList<Astronauta> alAstros = new ArrayList<>();
 		Statement statement = this.con.createStatement();
 		@SuppressWarnings("unused")
 		boolean resultado = statement.execute
-		("SELECT * FROM astronauta ORDER BY idAstronauta");
-			try (ResultSet resultSet = statement.getResultSet()) {
-				while (resultSet.next()){
-					int id = resultSet.getInt("idAstronauta");
-					String nome = resultSet.getString("Primeiro_Nome");
-					String nome_do_meio = resultSet.getString("Nome_do_Meio");
-					String sobrenome = resultSet.getString("Sobrenome");
-					String pais = resultSet.getString("Pais_Nasc");
-					String estado = resultSet.getString("Estado_Nasc");
-					String cidade = resultSet.getString("Cidade_Nasc");
-					String sexo = resultSet.getString("Sexo");
-					Date dataNasc = resultSet.getDate("DtNasc");
-					Date dataFalec = resultSet.getDate("DtFalec");
-					String foto = resultSet.getString("Foto");
-					String info = resultSet.getString("Info");
-					String info_eng = resultSet.getString("Info_eng");
-					ArrayList<String> missao = new ArrayList<String>();
-					for (int i = 1; i<=8; i++) {
-						missao.add(resultSet.getString("m" + i));
-					}
-					int n_missoes = resultSet.getInt("N_missoes");
-					Blob imagem = resultSet.getBlob("Imagem");
-					
-					Astronauta a = new Astronauta(
-							id, 
-							nome, 
-							nome_do_meio, 
-							sobrenome, 
-							pais, 
-							estado, 
-							cidade, 
-							foto, 
-							sexo, 
-							info, 
-							info_eng, 
-							dataNasc, 
-							dataFalec, 
-							missao,
-							n_missoes, 
-							imagem);
-		
-					alAstros.add(a);
-			}
-				resultSet.close();
-			} catch (Exception e) {
-				System.out.println("Erro de acesso ao Banco de Dados");
-				e.printStackTrace();
+				("SELECT * FROM astronauta ORDER BY idAstronauta");
+		try (ResultSet resultSet = statement.getResultSet()) {
+			while (resultSet.next()){
+				int id = resultSet.getInt("idAstronauta");
+				String nome = resultSet.getString("Primeiro_Nome");
+				String nome_do_meio = resultSet.getString("Nome_do_Meio");
+				String sobrenome = resultSet.getString("Sobrenome");
+				String pais = resultSet.getString("Pais_Nasc");
+				String estado = resultSet.getString("Estado_Nasc");
+				String cidade = resultSet.getString("Cidade_Nasc");
+				String sexo = resultSet.getString("Sexo");
+				LocalDate dataNasc = resultSet.getDate("DtNasc").toLocalDate();
+				LocalDate dataFalec;
+
+				try {
+					// tenta carregar data de falecimento
+					dataFalec = resultSet.getDate("DtFalec").toLocalDate();
 				}
-			return alAstros;
+				catch (NullPointerException ex) {
+					// caso a data de falecimento seja nula,
+					// carrega valor futuro que será usado para
+					// teste na execução de AstronautaGUI
+					dataFalec = LocalDate.of(2200, 1, 1);
+				}
+
+				String foto = resultSet.getString("Foto");
+				String info = resultSet.getString("Info");
+				String info_eng = resultSet.getString("Info_eng");
+				ArrayList<String> missao = new ArrayList<>();
+				for (int i = 1; i<=8; i++) {
+					missao.add(resultSet.getString("m" + i));
+				}
+				int n_missoes = resultSet.getInt("N_missoes");
+				Blob imagem = resultSet.getBlob("Imagem");
+
+				Astronauta a = new Astronauta(
+						id,
+						nome,
+						nome_do_meio,
+						sobrenome,
+						pais,
+						estado,
+						cidade,
+						foto,
+						sexo,
+						info,
+						info_eng,
+						dataNasc,
+						dataFalec,
+						missao,
+						n_missoes,
+						imagem);
+
+				alAstros.add(a);
+			}
+			resultSet.close();
+		} catch (Exception e) {
+			System.out.println("Erro de acesso ao Banco de Dados");
+			e.printStackTrace();
+		}
+		return alAstros;
 	}
-	
+
 	public ArrayList<Pais> pegaPaises()
 			throws SQLException {
 		ArrayList<Pais> alPaises = new ArrayList<>();
 		Statement statement = this.con.createStatement();
 		@SuppressWarnings("unused")
 		boolean resultado = statement.execute
-		("SELECT * FROM paises");
-			try (ResultSet resultSet = statement.getResultSet()) {
-				while (resultSet.next()){
-					String id = resultSet.getString("iso3");
-					String nome = resultSet.getString("nome");
-					
-					Pais pais = new Pais(id, nome);
-		
-					alPaises.add(pais);
+				("SELECT * FROM paises");
+		try (ResultSet resultSet = statement.getResultSet()) {
+			while (resultSet.next()){
+				String id = resultSet.getString("iso3");
+				String nome = resultSet.getString("nome");
+
+				Pais pais = new Pais(id, nome);
+
+				alPaises.add(pais);
 			}
-				resultSet.close();
-			} catch (Exception e) {
-				System.out.println("Erro de acesso ao Banco de Dados");
-				e.printStackTrace();
-				}
-			return alPaises;
+			resultSet.close();
+		} catch (Exception e) {
+			System.out.println("Erro de acesso ao Banco de Dados");
+			e.printStackTrace();
+		}
+		return alPaises;
 	}
 
 	public void adiciona(Astronauta astronauta) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public boolean remove(Astronauta astronauta) throws SQLException{
 		// TODO: implementar remocao de astronauta
 		int ConfirmaExclusao = JOptionPane.showConfirmDialog(null, "Confirma a exclusão do astronauta: " + astronauta + " ?", "Exclusão de Registro", JOptionPane.QUESTION_MESSAGE);
 		if (ConfirmaExclusao == 1) try {
 			Statement statement = this.con.createStatement();
 			boolean resultado = statement.execute
-			("REMOVE FROM astronauta WHERE IdAstronauta = " + astronauta.getIdAstronauta());
+					("REMOVE FROM astronauta WHERE IdAstronauta = " + astronauta.getIdAstronauta());
 			if (resultado) {
 				System.out.println("Registro excluido com sucesso.");
 				statement.close();
-				return true;} 
-					
-				else {
-							System.out.println("Erro de acesso ao banco de dados! Não foi possível excluir o registro.");
-							return false;
-			}
-			
-		} catch (Exception e) {
-				System.out.println("Erro de acesso ao Banco de Dados");
-				e.printStackTrace();
-				}
-		return false;
-		} 
-		
-		
-	
+				return true;}
 
-	public static final Astronauta NENHUM 
-		= new Astronauta(0, "SEM REGISTRO", "", "", "", "", "", 
-				"anonymous.jpg", "M", "", "", java.sql.Date.valueOf("1900-01-01"), 
-				java.sql.Date.valueOf("2100-01-01"), new ArrayList<String>(), 0, null);
+			else {
+				System.out.println("Erro de acesso ao banco de dados! Não foi possível excluir o registro.");
+				return false;
+			}
+
+		} catch (Exception e) {
+			System.out.println("Erro de acesso ao Banco de Dados");
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+
+
+
+	public static final Astronauta NENHUM
+			= new Astronauta(0, "SEM REGISTRO", "", "", "", "", "",
+			"anonymous.jpg", "M", "", "", LocalDate.of(1900, 1, 1),
+			LocalDate.of(2100, 1, 1), new ArrayList<>(), 0, null);
 
 }
